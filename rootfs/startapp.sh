8cb4b2501a8011de9650985817119a285363d8be
Jocelyn Le Sage <jocelyn@le-sage.com>
2024-06-30 23:14:00 -0400

    Added vim modeline.

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -6,3 +6,5 @@ set -u # Treat unset variables as an error.
 export HOME=/config
 cd /config
 exec /usr/bin/thunderbird -profile /config/profile >> /config/log/thunderbird/output.log 2>> /config/log/thunderbird/error.log
+
+# vim:ft=sh:ts=4:sw=4:et:sts=4

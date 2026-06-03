66019f450c37be07ea1413602b4d8e1b3fa6c3a4
Jocelyn Le Sage <jocelyn@le-sage.com>
2026-06-03 13:14:38 -0400

    Upgraded baseimage to version 4.12.3.

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -3,7 +3,6 @@
 set -e # Exit immediately if a command exits with a non-zero status.
 set -u # Treat unset variables as an error.
 
-export HOME=/config
 cd /config
 exec /usr/bin/thunderbird -profile /config/profile >> /config/log/thunderbird/output.log 2>> /config/log/thunderbird/error.log
 
